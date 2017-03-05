8f88214697709a9cec224a4b5ca1e4af1d65e3b6
Mohamed Toure <m.toure593@gmail.com>
2017-03-05 19:07:36 +0100

    Creation of “Santé Sans Frontière” website

---
+++ resources/views/website/index.blade.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Association humanitaire / promotion de la santé dans les pays en voie de développement / actions humanitaires">
+    <meta name="author" content="SSF">
+
+    <title>Santé Sans Frontière</title>
+
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    <link href="https://www.leetchi.com/Payment/MerchantFrame.css" rel="stylesheet" type="text/css"/>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+<!--Navigation bar-->
+
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">
+                <img src="img/logo.png" alt="logo"></i>
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li><a href="#qui-sommes-nous">Qui somme-nous ?</a></li>
+                <li><a href="#nos-projets">Nos projets</a></li>
+                <li><a href="#actualité">Actu</a></li>
+                <li><a href="#nous-aider">Nous aider</a></li>
+                <li><a href="#contact">Contactez-nous</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+</div>
+<!-- /.container-fluid -->
+</nav>
+
+<!-- Header -->
+<section id="header">
+    <div class="container-fluid">
+        <div class="dropdown">
+
+            <a href="#qui-sommes-nous" class="page-scroll btn btn-xl">En savoir plus</a>
+        </div>
+    </div>
+</section>
+
+<!-- Qui somme-nous Section -->
+<section id="qui-sommes-nous" class="bg-light-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Qui somme-nous?</h2>
+                <h3 class="section-subheading text-muted">L'association Santé Sans Frontière a pour objectif la promotion de la santé dans les pays en voie de développement, notamment dans les villages où l'accès aux soins est difficile, par le biais d’actions humanitaires.</h3>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-user-md fa-stack-1x fa-inverse"></i>
+                    </span>
+                <h4 class="nous-heading">Un regroupement de professionnels de santé</h4>
+                <p class="text-muted"> Externe et Interne en médecine, infirmiers, aide-soignantes, auxiliaires de puériculture, éducatrice spécialisée, secrétaire médicale...</p>
+            </div>
+            <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-heartbeat fa-stack-1x fa-inverse"></i>
+                    </span>
+                <h4 class="nous-heading">Pour rendre accessible la santé</h4>
+                <p class="text-muted">Par la mise en place de diverses actions humanitaires afin d'améliorer l'état de santé des populations défavorisées.</p>
+            </div>
+            <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-medkit fa-stack-1x fa-inverse"></i>
+                    </span>
+                <h4 class="nous-heading">Pour répondre au besoin d'éducation de la santé</h4>
+                <p class="text-muted">En leur permettant de devenir acteur de leur propre santé et ainsi éviter de contracter des maladies évitables.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Projets Section -->
+<section id="nos-projets">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Nos projets</h2>
+                <h3 class="section-subheading text-muted">La pluralité de tous les membres de cette association est un atout majeur, et cela va nous permettre de mettre en place différentes actions notamment:</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="action">
+                    <li>
+                        <div class="action-image">
+                            <img class="img-circle img-responsive" src="img/doctor-medical.jpeg" alt="">
+                        </div>
+                        <div class="action-panel">
+                            <div class="action-heading">
+                                <h4 class="subheading">La formation des professionnels locaux</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="action-inverted">
+                        <div class="action-image">
+                            <img class="img-circle img-responsive" src="img/africa-school.jpg" alt="">
+                        </div>
+                        <div class="action-panel">
+                            <div class="action-heading">
+                                <h4 class="subheading">L'éducation à la Santé</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="action-image">
+                            <img class="img-circle img-responsive" src="img/charity.jpg" alt="">
+                        </div>
+                        <div class="action-panel">
+                            <div class="action-heading">
+                                <h4 class="subheading">Mener des campagnes de prévention, de vaccination</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="action-inverted">
+                        <div class="action-image">
+                            <img class="img-circle img-responsive" src="img/diabetic-medicine.jpeg" alt="">
+                        </div>
+                        <div class="action-panel">
+                            <div class="action-heading">
+                                <h4 class="subheading">Faire des dons de dispositifs médicaux</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="action-image">
+                            <img class="img-circle img-responsive" src="img/medicine.jpg" alt="">
+                        </div>
+                        <div class="action-panel">
+                            <div class="action-heading">
+                                <h4 class="subheading">Promouvoir les produits locaux ...</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="action-inverted">
+                        <div class="action-image">
+                            <h4>Ensemble,
+                                <br>faisons bouger
+                                <br>les choses !!!</h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Actu Section -->
+<section id="actualité" class="bg-light-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Actualité</h2>
+                <h3 class="section-subheading text-muted">Suivez nos actions et nos évènements à venir</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="text-center">
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAssociation-Sant%25C3%25A9-Sans-Fronti%25C3%25A8res-1802614869957105%2F%3Ffref%3Dts&tabs=timeline%2C%20events&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Don Section -->
+<section id="nous-aider" >
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">VOUS POUVEZ NOUS AIDER !</h2>
+                <h3 class="section-subheading text-muted">Voici comment vous pouvez nous aider :</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="aider">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-comments-o fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4>Faites-nous un dons</h4>
+                    <p class="lead">
+                        Nous avons mis en place une cagnotte sur Leetchi afin que chacune de vos âmes charitables puisse faire un don, quelque soit la somme, pour que nous puissons atteindre nos objectifs.
+                    </p>
+                    <p class="lead">
+                        Nous vous remercions par avance du geste que vous effectuerez et espérons que comme nous, ces actions vous tiennent à coeur.
+                    </p>
+                    <a href="https://www.leetchi.com/c/association-de-diame">
+                        <button type="button" class="btn btn-primary btn-lg">
+                            Faire un don
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="aider">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-heart fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4>Parler de nous autour de vous</h4>
+                    <p class="lead">
+                        Diffusez ce site ou nos réseaux sociaux à a vos proches, votre médecin, votre pharmacien... afin de promouvoir la promotion de la santé dans les pays en voie de développement.
+                    </p>
+                        <a href="https://www.facebook.com/Association-Sant%C3%A9-Sans-Fronti%C3%A8res-1802614869957105/?fref=ts"><i class="fa fa-facebook fa-2x"></i></a>
+                        <a href="https://www.instagram.com/sante_sans_frontieres/?hl=fr"><i class="fa fa-instagram fa-2x"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Clients Aside -->
+<aside class="slogan bg-darkest-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <p>Ensemble, faisons bouger les choses !!!</p>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <p>Parce que la santé doit être accessible à tous!!!</p>
+            </div>
+            <div class="col-md-12 col-sm-12">
+                <p>LA SANTÉ EST SANS FRONTIÈRES!!!!</p>
+            </div>
+        </div>
+    </div>
+</aside>
+
+<!-- Contact Section -->
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Suivez-nous</h2>
+                <h3 class="section-subheading text-muted">Suivez nos actions sur nos différents réseaux sociaux et n'hésitez pas à nous contacter si vous souhaitez plus d'information</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <img src="img/village.jpg" class="img-fluid img-rounded" alt="village">
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <span class="copyright">Copyright &copy; Association Sans Frontières 2017</span>
+            </div>
+            <div class="col-md-6">
+                <ul class="list-inline social-buttons">
+                    <li><a href="https://www.instagram.com/sante_sans_frontieres/?hl=fr"><i class="fa fa-instagram"></i></a>
+                    </li>
+                    <li><a href="https://www.facebook.com/Association-Sant%C3%A9-Sans-Fronti%C3%A8res-1802614869957105/"><i class="fa fa-facebook"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- jQuery -->
+<script src="{{asset('js/jquery.js')}}"></script>
+
+<!-- Bootstrap -->
+<script src="{{asset('js/bootstrap.js')}}"></script>
+<script src="{{asset('js/bootstrap-select.js')}}"></script>
+<!-- Custom JavaScript -->
+<script src="{{asset('js/script.js')}}"></script>
+
+<script type="text/javascript" src="https://www.leetchi.com/Payment/MerchantFrame.js"></script>
+
+</body>
+
+</html>
